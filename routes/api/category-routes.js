bd9700e02ae19403ea347aee01efcc7b946c5f01
Val Brisendine <vsbrisendine@gmail.com>
2021-11-10 11:56:34 -0600

    still working with the routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,28 +2,37 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-// use mini project for ref ///////////////////////////////////////// try catch & async await 
+// use mini project for ref ///////////////////////////////////////// try catch & async await ***
 router.get('/', async (req, res) => {
-  // find all categories - findAll 
-  // be sure to include its associated Products
+  // find all categories - findAll - define data - and join [[[[[[[[[[[[[[[[[[[[[[[[ all ]]]]]]]]]]]]]]]]]]]]]]]]
+  // be sure to include** its associated Products - include : [{model: Product}] - route to models - Product.js 
   try {
-    const categoryData = await Catagory.findAll({
+    const categoryData = await Category.findAll({
       include: [{ model: Product }]
-    })
+    });
     res.status(200).json(categoryData);
- //88888888888888888888888888888888888
+    //88888888888888888888888888888888888\\
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 router.get('/:id', async (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  // find one category by its `id` value [[[[[[[[[[[[[[[[[[[[[[[[ id ]]]]]]]]]]]]]]]]]]]]]]]]
+  // be sure to include** its associated Products - include : [{model: Product}] - route to models - Product.js
   try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+    if (!travellerData) {
+      res.status(404).json({ message: 'No traveller found with this id!' });
+      return;
+    }
 
-  } catch {
-
+    res.status(200).json(travellerData);
+    //88888888888888888888888888888888888\\
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
@@ -36,7 +45,23 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  // delete a category by its `id` value. where: id 
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
