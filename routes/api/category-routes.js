66e199a73ece3c574cd51e8b0696ed34c2bd371e
Val Brisendine <vsbrisendine@gmail.com>
2021-11-10 09:19:28 -0600

    adding routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,15 +2,29 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
-  // find all categories
+// use mini project for ref ///////////////////////////////////////// try catch & async await 
+router.get('/', async (req, res) => {
+  // find all categories - findAll 
   // be sure to include its associated Products
+  try {
+    const categoryData = await Catagory.findAll({
+      include: [{ model: Product }]
+    })
+    res.status(200).json(categoryData);
+ //88888888888888888888888888888888888
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+
+  } catch {
+
+  }
 });
 
 router.post('/', (req, res) => {
@@ -18,7 +32,7 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  // update a category by its `id` value ----------- dont have ref for put =--- look up
 });
 
 router.delete('/:id', (req, res) => {
